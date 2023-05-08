5c203a6f09f07be1d74849f39d174b9fd56cc7ef
quyda100 <quyda100@gmail.com>
2023-05-08 21:20:46 +0700

    chage Password

--- auth/Interfaces/IAccountService.cs
+++ auth/Interfaces/IAccountService.cs
@@ -9,7 +9,7 @@ namespace auth.Interfaces
         public Task<IdentityResult> RegisterAsync(RegisterRequest model);
         public Task<IdentityResult> RegisterAdminAsync(RegisterRequest model);
         public Task<string> LoginAsync(LoginRequest model);
-        public Task<IdentityResult> ChangePassword(ChangepasswordRequest model)
+        public Task<IdentityResult> ChangePassword(ChangepasswordRequest model);
         public User GetUserByEmail(string email);
     }
 }
