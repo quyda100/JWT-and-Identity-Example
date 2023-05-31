54cbb790929bdddc092bf1bb0a82ce50849af8ca
Vương Nguyễn <vuong.nguyen.lifetechvn@gmail.com>
2023-05-31 16:07:51 +0700

    finish Payment

--- auth/Interfaces/INganLuongService.cs
+++ auth/Interfaces/INganLuongService.cs
@@ -2,7 +2,8 @@
 {
     public interface INganLuongService
     {
-        public string BuildCheckOutURL(string transactionInfo, string orderId, string price);
+        public string BuildCheckOutURL(string transactionInfo, string orderId, int price);
         public bool VerifyPaymentUrl(string transaction_info, string order_code, string price, string payment_id, string payment_type, string error_text, string secure_code);
+        public Task UpdateOrder(string order_code, string payment_id, string payment_type, string secure_code, string transaction_info);
     }
 }
